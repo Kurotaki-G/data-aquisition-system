fa1a3fb7a0ebb1772f6f48fb587084460209d3b2
Kurotaki-G <81437746+Kurotaki-G@users.noreply.github.com>
2024-07-02 01:53:35 +0000

    Correção confere id_novo(). Identificação a partir da tentativa de abertura do arquivo

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,6 @@ std::vector<std::string> split_string(std::string str, const char* op) {
 		operator_position = rest.find_first_of(op);
 		result.push_back(block);
 	}
-	
 	if (rest.length() > 0)
 		result.push_back(rest);
 		
@@ -56,22 +55,137 @@ struct LogRecord {
 };
 #pragma pack(pop)
 
-//declaração das funções 
+//declaração das funções próprias
 int write_in_file(std::string msg_sensor_id, std::string msg_time, std::string msg_value);
 std::string read_from_file(std::string sensor_id, int num_datas);
+bool id_novo(std::string sensor_id);
+std::string read_file(std::string sensor_id, int num_records_req);
 
-std::vector<std::string> ids;
-bool id_novo(std::string id){
-  for(int i=0;i<ids.size();i++){
-    if(id==ids[i]){
-      return true;
-    }
+
+class session
+  : public std::enable_shared_from_this<session>
+{
+public:
+  session(tcp::socket socket)
+    : socket_(std::move(socket))
+  {
   }
-  return false;
+
+  void start()
+  {
+    read_message();
+  }
+
+private:
+  void read_message()
+  {
+    auto self(shared_from_this());
+    boost::asio::async_read_until(socket_, buffer_, "\r\n",
+        [this, self](boost::system::error_code ec, std::size_t length)
+        {
+          if (!ec)
+          {
+            std::istream is(&buffer_);
+            std::string message(std::istreambuf_iterator<char>(is), {});
+            std::vector<std::string> new_message=split_string(message,"|");
+           std::string msg_type=new_message[0];
+           std::string id=new_message[1];
+           if(msg_type=="LOG"){
+              std::string data_hora = new_message[2];
+              std::string leitura = new_message[3];
+          }
+           if(msg_type=="GET"){
+            if(!id_novo(id)){
+              std::string aux=new_message[2];
+              int n_infos =stoi(aux.substr(0,aux.size()-4));
+              std::string rec = read_file(id, n_infos);
+              write_message(rec);
+            }
+            else{
+              //Envio de mensagem de erro para o cliente
+              std::string error = "ERROR|INVALID_SENSOR_" + id + "\r\n";
+              write_message(error);
+            }
+           }
+        }
+      
+        });
+  }
+
+  void write_message(const std::string& message)
+  {
+    auto self(shared_from_this());
+    boost::asio::async_write(socket_, boost::asio::buffer(message),
+        [this, self, message](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            read_message();
+          }
+        });
+  }
+
+  tcp::socket socket_;
+  boost::asio::streambuf buffer_;
+};
+
+class server
+{
+public:
+  server(boost::asio::io_context& io_context, short port)
+    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+  {
+    accept();
+  }
+
+private:
+  void accept()
+  {
+    acceptor_.async_accept(
+        [this](boost::system::error_code ec, tcp::socket socket)
+        {
+          if (!ec)
+          {
+            std::make_shared<session>(std::move(socket))->start();
+          }
+
+          accept();
+        });
+  }
+
+  tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Usage: chat_server <port>\n";
+    return 1;
+  }
+
+  boost::asio::io_context io_context;
+  server s(io_context, std::atoi(argv[1]));
+  io_context.run();
+
+  return 0;
 }
 
 
-// int write_in_file(std::string SensorName, std::string msg_time, std::string msg_value)
+
+/// FUNÇÕES PRÓPRIAS
+
+bool id_novo(std::string sensor_id){
+  std::ofstream ofs;
+	ofs.open(sensor_id+".dat",std::ios_base::in); //tenta abrir
+	if(!ofs)
+	{//caso não exista, avisa que é novo
+    ofs.close();
+    return true;
+	}
+  return false;
+}
+
 int write_in_file(std::string msg_sensor_id, std::string msg_time, std::string msg_value)
 {
 	// Abre o arquivo para leitura e escrita em modo binário e coloca o apontador do arquivo
@@ -163,125 +277,9 @@ std::string read_file(std::string sensor_id, int num_records_req)
 	else
 	{
 		std::cout << "Error opening file!" << std::endl;
+    return "ERROR";
 		
 	}
 	return(0);
 
-}
-
-
-
-class session
-  : public std::enable_shared_from_this<session>
-{
-public:
-  session(tcp::socket socket)
-    : socket_(std::move(socket))
-  {
-  }
-
-  void start()
-  {
-    read_message();
-  }
-
-private:
-  void read_message()
-  {
-    auto self(shared_from_this());
-    boost::asio::async_read_until(socket_, buffer_, "\r\n",
-        [this, self](boost::system::error_code ec, std::size_t length)
-        {
-          if (!ec)
-          {
-            std::istream is(&buffer_);
-            std::string message(std::istreambuf_iterator<char>(is), {});
-            std::vector<std::string> new_message=split_string(message,"|");
-           std::string msg_type=new_message[0];
-           std::string id=new_message[1];
-           if(msg_type=="LOG"){
-          }
-           if(msg_type=="GET"){
-
-            std::string aux=new_message[2];
-              int n_infos =stoi(aux.substr(0,aux.size()-4));
-              
-              std::string rec = read_file(id, n_infos);
-
-              write_message(rec);
-
-            if(!id_novo(id)){
-              
-
-            }
-            if(id_novo(id)){
-            //Envio de mensagem de erro para o cliente
-            std::string error = "ERROR|INVALID_SENSOR_" + id + "\r\n";
-            write_message(error);
-            }
-           }
-        }
-      
-        });
-  }
-
-
-
-  void write_message(const std::string& message)
-  {
-    auto self(shared_from_this());
-    boost::asio::async_write(socket_, boost::asio::buffer(message),
-        [this, self, message](boost::system::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            read_message();
-          }
-        });
-  }
-
-  tcp::socket socket_;
-  boost::asio::streambuf buffer_;
-};
-
-class server
-{
-public:
-  server(boost::asio::io_context& io_context, short port)
-    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
-  {
-    accept();
-  }
-
-private:
-  void accept()
-  {
-    acceptor_.async_accept(
-        [this](boost::system::error_code ec, tcp::socket socket)
-        {
-          if (!ec)
-          {
-            std::make_shared<session>(std::move(socket))->start();
-          }
-
-          accept();
-        });
-  }
-
-  tcp::acceptor acceptor_;
-};
-
-int main(int argc, char* argv[])
-{
-  if (argc != 2)
-  {
-    std::cerr << "Usage: chat_server <port>\n";
-    return 1;
-  }
-
-  boost::asio::io_context io_context;
-  server s(io_context, std::atoi(argv[1]));
-  io_context.run();
-
-  return 0;
 }
